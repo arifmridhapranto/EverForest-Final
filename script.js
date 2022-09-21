f201e8df8db6f7c24c14618689ce5e270040d400
arifmridhapranto <mridhapranto647@gmail.com>
2022-09-22 00:23:56 +0600

    refresh reloaded

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const navLinks = document.querySelectorAll(".nav-item");
-const menuToggle = document.getElementById("navbarSupportedContent");
-const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
-navLinks.forEach((l) => {
-  l.addEventListener("click", () => {
-    bsCollapse.toggle();
-  });
+// const navLinks = document.querySelectorAll(".nav-item");
+// const menuToggle = document.getElementById("navbarSupportedContent");
+// const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+// navLinks.forEach((l) => {
+//   l.addEventListener("click", () => {
+//     bsCollapse.toggle();
+//   });
+// });
+
+
+
+var dataSpyList = [].slice.call(
+  document.querySelectorAll('[data-bs-spy="scroll"]')
+);
+dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
 });
+
