329fec68337e1f40156d0c2a933578d229bb41af
Arif Mridha Pranto <mridhapranto647@gmail.com>
2022-09-22 15:54:39 +0600

    revision 5

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-// const navLinks = document.querySelectorAll(".nav-item");
-// const menuToggle = document.getElementById("navbarSupportedContent");
-// const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
-// navLinks.forEach((l) => {
-//   l.addEventListener("click", () => {
-//     bsCollapse.toggle();
-//   });
-// });
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+}
 
-
-
-var dataSpyList = [].slice.call(
-  document.querySelectorAll('[data-bs-spy="scroll"]')
-);
-dataSpyList.forEach(function (dataSpyEl) {
-  bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh();
-});
 
